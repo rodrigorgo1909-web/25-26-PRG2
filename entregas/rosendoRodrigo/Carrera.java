2ef2c9e02ecfdca275c24ded02d284b84f3b233d
rodrigorgo1909-web <rodrigorgo1909@gmail.com>
2026-04-24 11:49:38 +0200

    añado clases privadas para la pista y añado los caballos y turnos

--- entregas/rosendoRodrigo/Carrera.java
+++ entregas/rosendoRodrigo/Carrera.java
@@ -1,7 +1,16 @@
 public class Carrera {
+ private Pista pista;
+ Private Caballo [] caballos;
+ Private Turno turno;
 
+	Public Carrera(){
+	pista = new pista();
+	caballos = new caballo (2);
+	Caballos [0] = new Caballo (1);
+	Caballos [1] = new Caballo (2);
+	turno = new Turno();
 
-
+}
 
     public void jugar() {
        
